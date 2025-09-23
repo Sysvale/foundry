4429f844f7c8746f7017e7710305cc91f756245b
RafaelGondi <rdias.ga@gmail.com>
2025-09-23 09:57:55 -0300

    feat: Implementa util sanitizeForm

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './utils/pluralize';
 export * from './utils/commaline';
+export * from './utils/sanitizeForm';
 export { maskCpf, removeCpfMask } from './formatters/cpf';
 export { maskPhone, removePhoneMask } from './formatters/phone';
