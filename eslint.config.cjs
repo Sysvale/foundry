0ad142bfdb793b7d3b88a925e3e600cefacb601a
RafaelGondi <rdias.ga@gmail.com>
2025-09-23 10:37:04 -0300

    style: corrige problemas apontados pelo prettier

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -39,12 +39,12 @@ module.exports = [
 		},
 	},
 	{
-	files: [
-		'src/**/*.{ts,tsx}',
-		'lib/**/*.{ts,tsx}',
-		'components/**/*.{ts,tsx}',
-		'tests/**/*.{ts,tsx}',
-	],
+		files: [
+			'src/**/*.{ts,tsx}',
+			'lib/**/*.{ts,tsx}',
+			'components/**/*.{ts,tsx}',
+			'tests/**/*.{ts,tsx}',
+		],
 		languageOptions: {
 			ecmaVersion: 'latest',
 			sourceType: 'module',
